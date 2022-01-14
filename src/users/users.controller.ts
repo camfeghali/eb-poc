3027a8a40329b8c68b8a3d6ed8baef3edd6e5ef6
camfeghali <camfeg@gmail.com>
2022-01-14 18:03:55 +0100

    added user service and controller logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
-@Controller('users')
-export class UsersController {}
+@Controller('/api/users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): Promise<User> {
+    return this.usersService.findById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(body);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findById(id: number): Promise<User> {
+    return this.usersRepository.findOneOrFail(id);
+  }
+
+  createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser); // insert
+  }
+}
