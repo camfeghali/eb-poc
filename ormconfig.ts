91655b3b49d78586a4ff0b76df4f49fe8e84c7f8
camfeghali <camfeg@gmail.com>
2022-01-15 11:49:58 +0100

    connects to rds

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,24 @@
 import { ConnectionOptions } from 'typeorm';
 require('dotenv').config({ path: './development.env' });
 
+const host = process.env.RDS_HOSTNAME || process.env.DB_HOST || 'localhost';
+const port =
+  parseInt(process.env.RDS_PORT, 10) ||
+  parseInt(process.env.DB_PORT, 10) ||
+  3306;
+const database = process.env.MYSQL_DATABASE || 'ebdb';
+const username =
+  process.env.RDS_USERNAME || process.env.MYSQL_USER || 'tenants-api';
+const password =
+  process.env.RDS_PASSWORD || process.env.MYSQL_PASSWORD || 'dummypwd';
+
 const config: ConnectionOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.MYSQL_USER || 'tenants-api',
-  password: process.env.MYSQL_PASSWORD || 'dummypwd',
-  database: process.env.MYSQL_DATABASE || 'tenants',
+  host,
+  port,
+  username,
+  password,
+  database,
   synchronize: false,
   entities: [__dirname + '/**/**/entities/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'], // where to read migrations from.
@@ -15,5 +26,7 @@ const config: ConnectionOptions = {
     migrationsDir: './migrations', // where to save created migrations to.
   },
 };
+
+console.log(config);
 
 export = config;
